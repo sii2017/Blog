ae5c444ae4d04fa5f82dab15b7b9b53e034e8227
sii2017 <30471043+sii2017@users.noreply.github.com>
2017-11-28 20:49:03 +0800

    Update

---
+++ C++/字符和字符串/main.c
@@ -0,0 +1,14 @@
+#include <windows.h>
+//#include <string>
+//#include <iostream>
+//using namespace std;
+
+int main(void)
+{
+	char buff[]= "We, should, delete, the, comma.";
+	
+	//cout << buff << endl;
+
+	getchar();
+	return 0;
+}
